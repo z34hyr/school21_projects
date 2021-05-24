03af111d06692cd97e452758b4831a95b0b4989b
z34hyr <75954688+z34hyr@users.noreply.github.com>
2021-05-24 06:31:20 +0300

    Add files via upload

--- 12_containers/includes/map.hpp
+++ 12_containers/includes/map.hpp
@@ -13,7 +13,7 @@
 #include <utility>
 
 #include "is_integer.hpp"
-#include "binary_search_tree.cpp" // OFC must change to hpp
+#include "binary_search_tree.hpp"
 
 namespace ft
 {
@@ -23,11 +23,12 @@ namespace ft
 	class map
 	{
 		public:
-			typedef bst<Key, T, Compare> bst_struct;
+			typedef bst<Key, T, Compare>					bst_struct;
 			typedef	typename bst_struct::key_type			key_type;
 			typedef typename bst_struct::mapped_type		mapped_type;
 			typedef	Compare									key_compare;
 			typedef	std::pair<const key_type,mapped_type>	value_type;
+			typedef bst_node<value_type>*					node_ptr;
 			typedef value_type&								reference;
 			typedef value_type const &						const_reference;
 			typedef value_type*								pointer;
@@ -37,6 +38,7 @@ namespace ft
 			typedef typename bst_struct::iterator			iterator;
 			typedef typename bst_struct::const_iterator	const_iterator;
 
+			// special for value_type comparator
 			template<typename arg1, typename arg2, typename result>
 			struct binary_res_function
 			{
@@ -68,24 +70,26 @@ namespace ft
 		public:
 			explicit map (const key_compare& comp = key_compare())
 			{
-				std::cout << "Empty constructor\n";
 			};//empty
 			template <class InputIterator>
 				map (InputIterator first, InputIterator last,
 				const key_compare& comp = key_compare())
 			{
-				std::cout << "Iterator constructor\n";
+				this->insert(first, last);
 			};//range
 			map (const map& x)
 			{
-				
-			};
+				this->operator=(x);
+			};//copy constructor
 			map& operator= (const map& x)
 			{
-				~holder;
-				holder();
-				
-			};
+				if (this != &x)
+				{
+					this->clear();
+					this->insert(x.begin(), x.end());
+				}
+				return *this;
+			};// assignment operator overload
 			~map() {};
 			iterator begin() { return holder.begin(); };
 			const_iterator begin() const { return holder.begin(); };
@@ -98,11 +102,9 @@ namespace ft
 
 			// CAPACITY
 			size_type	size() const { return holder.get_size(); };
-			size_type	max_size() const
-			{
+			size_type	max_size() const { return std::numeric_limits<size_type>::max() / sizeof(value_type); };
 
-			};
-			bool		empty() const { return (holder.get_size() ? true : false); };
+			bool		empty() const { return (!holder.get_size() ? true : false); };
 
 			///////////////////////////////////////////////////////////////////////
 			///				ELEMENTS ACCESS.								///////
@@ -139,23 +141,22 @@ namespace ft
 			};// single element by iterator
 			size_type erase (const key_type& k)
 			{
-				holder.remove(k);
-				return holder.get_size();
+				return holder.remove(k);
 			};// single element by key
 			void erase (iterator first, iterator last)
 			{
 				while (first != last)
 					erase(first++);
-			};// range by iteratorors diapasone
+			};// range by iterators diapasone
 			void		swap (map& x)
 			{
-
+				holder.swap(x.holder);
 			};
 			void		clear()
 			{
 				holder.clear();
 			};
-
+			
 			///////////////////////////////////////////////////////////////////////
 			///				OBSERVERS										///////
 			///////////////////////////////////////////////////////////////////////
@@ -204,62 +205,15 @@ namespace ft
 			{
 				
 			};
+			std::pair<iterator, iterator>             equal_range (const key_type& k)
+			{
+				iterator lb = lower_bound(k);
+				return std::pair<iterator, iterator>(lb, lb);
+			};
 			std::pair<const_iterator, const_iterator> equal_range (const key_type& k) const
 			{
 				const_iterator lb = lower_bound(k);
 				return std::pair<const_iterator, const_iterator>(lb, lb);
 			};
-			std::pair<iterator, iterator>             equal_range (const key_type& k)
-			{
-				iterator lb = lower_bound(k);
-				std::pair<iterator, iterator> rez(lb, lb);
-				return rez;
-			};
-
-		private:
-			template<typename InputIterator>
-			void
-			my_initialize_dispatch(InputIterator first, InputIterator last,
-				false_type)
-			{
-
-			}
-			template<typename Integer>
-			void
-			my_initialize_dispatch(Integer n,
-			   Integer val, true_type)
-			{
-
-			}
-			void	init_iter(iterator first, iterator last)
-			{
-
-			}
-			void	init_int(size_type n, value_type const & val)
-			{
-
-			}
-			template<typename InputIterator>
-			void
-			my_insert_dispatch(iterator position, InputIterator __first,
-			   InputIterator __last, false_type)
-			{
-
-			}
-			template<typename Integer>
-			void
-			my_insert_dispatch(iterator position, Integer n,
-			   Integer val, true_type)
-			{
-
-			}
-			void	insert_int(iterator position, size_type n, const value_type& val)
-			{
-
-			}
-			void	insert_iter(iterator position, size_type n, iterator first)
-			{
-
-			}
 	};
 }
